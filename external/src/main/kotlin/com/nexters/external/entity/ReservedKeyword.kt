be9e0ecfd8b63881fa6a63d0d5530d285818dd94
seongahjo <ah.jo@navercorp.com>
2025-07-19 13:29:02 +0900

    불필요한 연관관계 제거

--- external/src/main/kotlin/com/nexters/external/entity/ReservedKeyword.kt
+++ external/src/main/kotlin/com/nexters/external/entity/ReservedKeyword.kt
@@ -5,9 +5,6 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.JoinTable
-import jakarta.persistence.ManyToMany
 import jakarta.persistence.Table
 import java.time.LocalDateTime
 
@@ -19,13 +16,6 @@ class ReservedKeyword(
     val id: Long? = null,
     @Column(nullable = false, unique = true)
     val name: String,
-    @ManyToMany
-    @JoinTable(
-        name = "keyword_mappings",
-        joinColumns = [JoinColumn(name = "reserved_keyword_id")],
-        inverseJoinColumns = [JoinColumn(name = "candidate_keyword_id")]
-    )
-    val candidateKeywords: MutableSet<CandidateKeyword> = mutableSetOf(),
     @Column(nullable = false)
     val createdAt: LocalDateTime = LocalDateTime.now(),
     @Column(nullable = false)
