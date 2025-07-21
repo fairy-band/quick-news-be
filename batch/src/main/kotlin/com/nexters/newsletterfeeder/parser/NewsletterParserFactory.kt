59b087a09a4992b4f6abfbcbc49ac42979100c31
her0807 <her0807@naver.com>
2025-07-21 23:28:13 +0900

    refactor: 네이밍 변경

--- batch/src/main/kotlin/com/nexters/newsletterfeeder/parser/NewsletterParserFactory.kt
+++ batch/src/main/kotlin/com/nexters/newsletterfeeder/parser/NewsletterParserFactory.kt
@@ -1,12 +1,11 @@
 package com.nexters.newsletterfeeder.parser
 
 object MailParserFactory {
-    private val parsers =
-        listOf(
-            ReactStatusParser(),
-            KotlinWeeklyParser(),
-            // 다른 파서들 추가
-        )
+    private val parsers = listOf(
+        ReactStatusParser(),
+        KotlinWeeklyParser(),
+        // 다른 파서들 추가
+    )
 
     fun from(sender: String): MailParser? = parsers.firstOrNull { it.isTarget(sender) }
 }
