a6161c1d1fe5fa453d60601e19197388e2d84adb
ah.jo <seongside@gmail.com>
2025-07-27 11:23:21 +0900

    헬스체크 추가

---
+++ batch/src/main/kotlin/com/nexters/newsletterfeeder/controller/HealthCheckController.kt
@@ -0,0 +1,16 @@
+package com.nexters.newsletterfeeder.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthCheckController {
+    
+    @GetMapping
+    fun healthCheck(): ResponseEntity<Map<String, String>> {
+        return ResponseEntity.ok(mapOf("status" to "UP"))
+    }
+} 
