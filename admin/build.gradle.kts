c7e9a49a372ff0bbfcedf2607fdd445f3d5040ce
her0807 <her0807@naver.com>
2025-08-01 19:18:47 +0900

    feat(admin): Spring Security 및 OAuth2 의존성 추가
    - spring-boot-starter-security 추가
    - spring-boot-starter-oauth2-client 추가
    - spring-boot-starter-security-test 추가
    - kotlin-logging 의존성 추가

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -6,11 +6,19 @@ dependencies {
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.data.mongodb)
+    implementation(libs.spring.boot.starter.security)
+    implementation(libs.spring.boot.starter.oauth2.client)
     implementation(libs.kotlin.reflect)
     implementation(libs.jackson.module.kotlin)
 
+    // Kotlin logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+
     testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.starter.security.test)
     testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.mockk)
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
     testRuntimeOnly(libs.junit.platform.launcher)
 }
 
