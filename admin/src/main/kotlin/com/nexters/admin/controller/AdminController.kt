4f4a336cf2221721c7b9b47b50026bb6bfbd5393
her0807 <her0807@naver.com>
2025-08-02 15:06:44 +0900

    스타일 수정

--- admin/src/main/kotlin/com/nexters/admin/controller/AdminController.kt
+++ admin/src/main/kotlin/com/nexters/admin/controller/AdminController.kt
@@ -1,7 +1,6 @@
 package com.nexters.admin.controller
 
 import mu.KotlinLogging
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
