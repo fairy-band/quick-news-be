58fb4dc020d5ff888f4fc386d5abcc18337e10ad
her0807 <her0807@naver.com>
2025-08-04 23:17:24 +0900

    refactor : 코드스타일

--- api/src/main/kotlin/com/nexters/api/controller/NotificationApiController.kt
+++ api/src/main/kotlin/com/nexters/api/controller/NotificationApiController.kt
@@ -7,7 +7,13 @@ import com.nexters.external.service.PushNotificationService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Notification", description = "푸시 알림 관련 API")
 @RestController
