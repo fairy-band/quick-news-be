06612bd1737c015b453223dfd91db1b4087cda58
ah.jo <seongside@gmail.com>
2025-08-14 20:25:15 +0900

    발행일을 가중치 계산에 포함

--- newsletter/src/main/kotlin/com/nexters/newsletter/resolver/RecommendScoreCalculator.kt
+++ newsletter/src/main/kotlin/com/nexters/newsletter/resolver/RecommendScoreCalculator.kt
@@ -1,5 +1,7 @@
 package com.nexters.newsletter.resolver
 
+import java.time.LocalDate
+
 class RecommendScoreCalculator {
     fun calculate(source: RecommendCalculateSource): RecommendCalculateResult {
         // 양수 가중치의 곱 계산
@@ -16,23 +18,30 @@ class RecommendScoreCalculator {
 
         // 최종 가중치 = 양수 가중치의 곱 + 음수 가중치의 합
         // 음수 가중치가 너무 크면 0으로 만들기 위해 max 사용
-        return RecommendCalculateResult(maxOf(positiveWeight - negativeWeight, 0.0))
+        val keywordScore = maxOf(positiveWeight - negativeWeight, 0.0)
+
+        val today = LocalDate.now()
+        val freshScore = (today.until(source.publishedDate).days) * 10 // 오늘이 아닐경우 항상 음수
+        val lastScore = maxOf(keywordScore + freshScore, 0.0)
+
+        return RecommendCalculateResult(lastScore)
     }
 }
 
 data class RecommendCalculateResult(
-    val recommendScore: Double
+    val recommendScore: Double,
 )
 
 data class RecommendCalculateSource(
     val positiveKeywordSources: List<PositiveKeywordSource>,
     val negativeKeywordSources: List<NegativeKeywordSource>,
+    val publishedDate: LocalDate,
 )
 
 data class PositiveKeywordSource(
-    val weight: Double
+    val weight: Double,
 )
 
 data class NegativeKeywordSource(
-    val weight: Double
+    val weight: Double,
 )
