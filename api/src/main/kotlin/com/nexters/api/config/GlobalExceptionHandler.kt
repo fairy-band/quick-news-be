a4faf02b18581154f0021ea2faaf74e880821395
ah.jo <ah.jo@navercorp.com>
2025-08-16 19:25:12 +0900

    유저 없을 때 404 반환하도록 수정 (#61)

---
+++ api/src/main/kotlin/com/nexters/api/config/GlobalExceptionHandler.kt
@@ -0,0 +1,17 @@
+package com.nexters.api.config
+
+import com.nexters.api.dto.ApiResponse
+import com.nexters.api.exception.UserNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException::class)
+    fun handleUserNotFoundException(ex: UserNotFoundException): ResponseEntity<ApiResponse<Nothing>> =
+        ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(ApiResponse(success = false, message = ex.message ?: "User not found"))
+}
