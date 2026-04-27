d983efbdd3bd9be51d3930d6225a47a48508f4ae
ah.jo <seongside@gmail.com>
2026-04-27 23:56:48 +0900

    컨텐츠 최대 길이 향상

--- external/src/main/kotlin/com/nexters/external/constants/ContentConstants.kt
+++ external/src/main/kotlin/com/nexters/external/constants/ContentConstants.kt
@@ -2,12 +2,12 @@ package com.nexters.external.constants
 
 object ContentConstants {
     /**
-     * 콘텐츠당 최대 길이 (약 20K-30K 토큰)
+     * 콘텐츠당 최대 길이
      */
-    const val MAX_CONTENT_LENGTH = 10_000
+    const val MAX_CONTENT_LENGTH = 100_000
 
     /**
      * 배치 전체 최대 길이
      */
-    const val MAX_TOTAL_BATCH_LENGTH = 50_000
+    const val MAX_TOTAL_BATCH_LENGTH = 500_000
 }
