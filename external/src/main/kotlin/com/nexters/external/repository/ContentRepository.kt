f2e822e7d03d2032f2d2d8ba95b62541cfc8af7f
조성아 <joseong-a@joseong-aui-Mac-Studio.local>
2026-08-23 21:33:52 +0900

    fix(repository): import java.time.LocalDateTime in ContentRepository

--- external/src/main/kotlin/com/nexters/external/repository/ContentRepository.kt
+++ external/src/main/kotlin/com/nexters/external/repository/ContentRepository.kt
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 interface ContentRepository : JpaRepository<Content, Long> {
     @Query(
